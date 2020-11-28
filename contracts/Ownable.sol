69d8f1e141472c57d9f6952495c6e670abdcb227
asjames18 <asjames18@gmail.com>
2020-11-27 20:29:39 -0500

    first commit

---
+++ contracts/Ownable.sol
@@ -0,0 +1,16 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Ownable{
+    address public owner;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _; //Continue execution
+    }
+
+    constructor() public{
+        owner = msg.sender;
+    }
+}
